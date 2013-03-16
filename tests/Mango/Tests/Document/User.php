3403090c02b53145bca9bb7e82bcec44410c55a2
David Henning <madcat.me@gmail.com>
2013-03-16 22:13:32 +0100

    added field config

--- tests/Mango/Tests/Document/User.php
+++ tests/Mango/Tests/Document/User.php
@@ -9,5 +9,33 @@ class User implements DocumentInterface
 {
     use Document;
 
+    public $created_at;
+    public $updated_at;
     public $name;
+
+    private function addFields()
+    {
+        $this->addField(
+            'name',
+            []
+        );
+
+        $this->addField(
+            'created_at',
+            [
+                'type' => 'DateTime',
+                'index' => true,
+                'default' => 'now'
+            ]
+        );
+
+        $this->addField(
+            'updated_at',
+            [
+                'type' => 'DateTime',
+                'index' => true,
+                'default' => 'now'
+            ]
+        );
+    }
 }
