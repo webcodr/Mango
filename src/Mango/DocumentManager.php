c1649763f13e86b0cf76c4d696a96feefd06b70b
David Henning <madcat.me@gmail.com>
2013-03-16 22:12:47 +0100

    removed checks due to introduction of the (De)Hydrator classes

--- src/Mango/DocumentManager.php
+++ src/Mango/DocumentManager.php
@@ -15,24 +15,14 @@ class DocumentManager
 
     public function store(DocumentInterface $document)
     {
-        if (!isset($document->_id) || !$document->_id instanceof \MongoId) {
-            throw new \InvalidArgumentException('Document does not contain a valid MongoID.');
-        }
-
         $collection = $this->connection->selectCollection($document::getCollectionName());
-
         $data = $document->getProperties()->getArray();
         $collection->save($data);
     }
 
     public function remove(DocumentInterface $document)
     {
-        if (!isset($document->_id) || !$document->_id instanceof \MongoId) {
-            throw new \InvalidArgumentException('Document does not contain a valid MongoID.');
-        }
-
         $collection = $this->connection->selectCollection($document::getCollectionName());
-
         $collection->remove($document->_id);
     }
 
